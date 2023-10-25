4875cfc664f78cfeeb36d0c0ea219ba361c70c34
Ryota Takada <takada.ryo@dinamica.co.jp>
2023-10-25 22:50:00 +0900

    add/ModalProvider

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import Modal from '@/components/Modal';
+import { useEffect, useState } from 'react';
+
+const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
+  return (
+    <>
+      <Modal
+        title='Test Title'
+        description='Test Description'
+        isOpen
+        onChange={() => {}}
+      >
+        Children
+      </Modal>
+    </>
+  );
+};
+
+export default ModalProvider;
