cd93ca7ec4cdb2bb4924751dc226f5fb6f1ab9ae
Ryota Takada <59213956+ryota-dev@users.noreply.github.com>
2023-10-27 22:30:17 +0900

    Feature/fetching songs (#9)
    * add/SongCard
    
    * add/マイライブラリ追加
    
    * add/検索機能
    
    ---------
    
    Co-authored-by: ryota.t.exe@gmail.com <takada.ryo@dinamica.co.jp>

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['anrwzziwkjjxuiqcbbbk.supabase.co'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
