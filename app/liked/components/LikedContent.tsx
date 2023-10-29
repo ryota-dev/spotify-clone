aa94745de3db2d6d85314be7d7e2d2347de44b68
Ryota Takada <59213956+ryota-dev@users.noreply.github.com>
2023-10-29 14:03:37 +0900

    Feature/player functionality (#11)
    * add/再生コンポーネント
    
    * add/再生機能
    
    * add/全体に再生機能適用
    
    ---------
    
    Co-authored-by: ryota.t.exe@gmail.com <takada.ryo@dinamica.co.jp>

--- app/liked/components/LikedContent.tsx
+++ app/liked/components/LikedContent.tsx
@@ -2,6 +2,7 @@
 
 import LikeButton from '@/components/LikeButton';
 import MediaItem from '@/components/MediaItem';
+import useOnPlay from '@/hooks/useOnPlay';
 import { useUser } from '@/hooks/useUser';
 import { Song } from '@/types';
 import { useRouter } from 'next/navigation';
@@ -14,6 +15,7 @@ interface LikedContentProps {
 const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
   const router = useRouter();
   const { isLoading, user } = useUser();
+  const onPlay = useOnPlay(songs);
 
   useEffect(() => {
     if (!isLoading && !user) router.push('/');
@@ -31,7 +33,7 @@ const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
       {songs.map((song) => (
         <div key={song.id} className='flex items-center gap-x-4 w-full'>
           <div className='flex-1'>
-            <MediaItem data={song} onClick={() => {}} />
+            <MediaItem data={song} onClick={(id: string) => onPlay(id)} />
           </div>
           <LikeButton songId={song.id} />
         </div>
