f1217a119cb95ea3f3a40e5f5e621d2e4390bd21
ryota.t.exe@gmail.com <takada.ryo@dinamica.co.jp>
2023-10-29 12:59:04 +0900

    add/再生コンポーネント

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 interface MediaItemProps {
   data: Song;
-  onClick: () => void;
+  onClick?: () => void;
 }
 
 const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
